aece1384676263690bfa9807b509958ef95b60a9
dzta <dzta@protonmail.com>
2024-10-30 09:49:07 +0100

    Add project files.

---
+++ Garage2/Data/Garage2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage2.Models;
+
+namespace Garage2.Data
+{
+    public class Garage2Context : DbContext
+    {
+        public Garage2Context (DbContextOptions<Garage2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage2.Models.ParkedVehicle> ParkedVehicle { get; set; } = default!;
+    }
+}
---
+++ Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace Garage2.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}

e6b9832d45a17f1c79f02c5bff330bbf2dbc723a
Zhark1561 <Simon.Haglund@futuregames.nu>
2024-10-30 13:33:07 +0100

    ParkedVehiclesViewModel

---
+++ Garage2/Models/ParkedVehiclesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage2.Models
+{
+    public class ParkedVehiclesViewModel
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime ParkedTime { get; set; }
+    }
+}

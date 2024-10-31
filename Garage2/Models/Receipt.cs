0df31b895658c84284bc71382008ec68698bad8a
dzta <dzta@protonmail.com>
2024-10-31 09:38:41 +0100

    Adding recipt class and view, removing checkout time från overview

---
+++ Garage2/Models/Receipt.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage2.Models
+{
+    public class Receipt
+    {
+        public string RegistrationNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public TimeSpan ParkingDuration => DepartureTime - ArrivalTime;
+        public decimal ParkingCost { get; set; }
+    }
+}
